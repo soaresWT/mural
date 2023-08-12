c72dd743b11501eb689385bbb5140200386a0544
soareswt <victorsuares2014@gmail.com>
2023-08-12 14:03:25 -0300

    bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home() {
     <>
       <Menu />
       <section className="h-auto w-screen p-0 m-0 flex flex-col items-center justify-center">
-        <h1 className='text-[#0052FE] mt-8 text-[4em] font-bold'> O que é o mural?</h1>
-        <div className="bg-pink-300 bg-opacity-60 backdrop-blur-sm flex rounded-[15px] items-center flex-col w-2/5 min-w-[400px] h-3/5 min-h-[400px] mt-8 shadow-lg">
+        <h1 className='text-[#0052FE] text-center p-3 mt-8 text-[3em] font-bold'> O que é o mural?</h1>
+        <div className="bg-pink-300 bg-opacity-60 backdrop-blur-sm flex rounded-[15px] items-center flex-col w-2/5 min-w-[250px] h-3/5 min-h-[400px] mt-8 shadow-lg">
           <div className='bg-[#0052FE] rounded-t-[15px] flex p-2 items-center w-full'>oi</div>
           <p>Conteúdo da primeira div...</p>
         </div>
-        <div className="bg-pink-300 bg-opacity-60 backdrop-blur-sm flex rounded-[15px] items-center flex-col w-2/5 min-w-[400px] h-3/5 min-h-[400px] mt-8 shadow-lg">
+        <div className="bg-pink-300 bg-opacity-60 backdrop-blur-sm flex rounded-[15px] items-center flex-col w-2/5 min-w-[250px] h-3/5 min-h-[400px] mt-8 shadow-lg">
           <div className='bg-[#0052FE] rounded-t-[15px] flex p-2 items-center w-full'>oi</div>
           <p>Conteúdo da primeira div...</p>
         </div>
-        <div className="bg-pink-300 bg-opacity-60 backdrop-blur-sm flex rounded-[15px] items-center flex-col w-2/5 min-w-[400px] h-3/5 min-h-[400px] mt-8 shadow-lg">
+        <div className="bg-pink-300 bg-opacity-60 backdrop-blur-sm flex rounded-[15px] items-center flex-col w-2/5 min-w-[250px] h-3/5 min-h-[400px] mt-8 shadow-lg">
           <div className='bg-[#0052FE] rounded-t-[15px] flex p-2 items-center w-full'>oi</div>
           <p>Conteúdo da primeira div...</p>
         </div>
